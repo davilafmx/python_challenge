ba2827efe0e3690d61b4ff79d3de7c910c4f6571
Jose Davila <139913551+davilafmx@users.noreply.github.com>
2023-10-19 02:15:46 -0500

    updates

---
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,7 @@
+import csv
+import os
+import pandas as pd
+
+bank_cvs = path("/resources/budget_data.csv")
+bank_df = pd.read_csv (bank_cvs, encoding="utf-8")
+
--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,16 @@
 
+import pandas as pd
+from pathlib import Path
+import locale
+
+bank_csv = Path("Resources/budget_data.csv")
+bank_df = pd.read_csv (bank_csv, encoding="utf-8")
+Total_months = bank_df["Date"].count()
+Total_months
+print("Total Months : ", Total_months)
+Total_sum = bank_df["Profit/Losses"].sum()
+locale.setlocale( locale.LC_ALL, '' )
+'English_United States.1252'
+locale.currency(Total_sum)
+print("Total Sum : ", locale.currency(Total_sum))
+
